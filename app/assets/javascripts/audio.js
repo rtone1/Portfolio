56ec667e1fd8a6ea9cff02ef438b2a138e8986f9
Rodrigo Torres <rtdesigns@yahoo.com>
2016-09-17 10:21:12 -0500

    added a third party directive to handle mobile dblclick but need a better solution later

--- app/assets/javascripts/audio.js
+++ app/assets/javascripts/audio.js
@@ -392,4 +392,40 @@
     }
   });
 
+  // THIRD PARTY DIRECTIVE TO HANDLE DOUBLE CLICKS ON MOBILE
+  // MAKE YOUR ON OR FIND A BETTER SOLUTION
+  app.directive('iosDblclick',
+    function () {
+
+        const DblClickInterval = 300; //milliseconds
+
+        var firstClickTime;
+        var waitingSecondClick = false;
+
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                element.bind('click', function (e) {
+
+                    if (!waitingSecondClick) {
+                        firstClickTime = (new Date()).getTime();
+                        waitingSecondClick = true;
+
+                        setTimeout(function () {
+                            waitingSecondClick = false;
+                        }, DblClickInterval);
+                    }
+                    else {
+                        waitingSecondClick = false;
+
+                        var time = (new Date()).getTime();
+                        if (time - firstClickTime < DblClickInterval) {
+                            scope.$apply(attrs.iosDblclick);
+                        }
+                    }
+                });
+            }
+        };
+    });
+
 })(); // END OF SELF CALL CLOSURE
